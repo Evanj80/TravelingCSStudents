29e9b9db9bc0cd197b81acaca22176220427d06a
alvinn45 <alvin.a.nguyen@sjsu.edu>
2021-11-30 11:01:45 -0800

    update script for bulgaria datasets

--- bulg-script.sh
+++ bulg-script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 python nearestneighbor.py;
-for i in {1..10}; do
-    echo === Start at $i ===;
+for i in {01..10}; do
     echo python nearestneighbor.py bulgaria-small.tsp bulgaria-small-edges.tsp $i;
     python nearestneighbor.py bulgaria-small.tsp bulgaria-small-edges.tsp $i > result-bsmall-$i.txt;
-    echo -e =============='\n';
 done
+tail -n15 result-bsmall*.txt > results-bsmall.txt
