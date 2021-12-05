cc253812b55873c9f8157cdb87d62ecb9a01c231
alvinn45 <alvin.a.nguyen@sjsu.edu>
2021-12-05 00:53:33 -0800

    rm old files

--- bulg-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python nearestneighbor.py;
-for i in {01..10}; do
-    echo python nearestneighbor.py bulgaria-small.tsp bulgaria-small-edges.tsp $i;
-    python nearestneighbor.py bulgaria-small.tsp bulgaria-small-edges.tsp $i > result-bsmall-$i.txt;
-done
-tail -n15 result-bsmall*.txt > results-bsmall.txt
--- nn-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python nearestneighbor.py;
-r=0;
-for i in {1..5}; do
-    echo === Test $i ===;
-    echo python nearestneighbor.py test$i-vertices.txt test$i-edges.txt $r;
-    python nearestneighbor.py test$i-vertices.txt test$i-edges.txt $r;
-    echo -e =============='\n';
-done
